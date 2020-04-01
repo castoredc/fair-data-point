be4b64b25683847eff310c9cfe393eebd050bd83
Martijn Kersloot <martijn@castoredc.com>
2020-04-01 10:18:49 +0200

    Display map on dataset page

--- src/js/components/MetadataItem/Organizations.js
+++ src/js/components/MetadataItem/Organizations.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 
 import './MetadataItem.scss'
 import MetadataItem from "./index";
+import {AttributionControl, Map, Marker, TileLayer} from "react-leaflet";
+import {getCenterFromDegrees} from "../../util";
 
 const Organization = ({name, url, type, email, center}) => {
     if(email)
@@ -40,10 +42,39 @@ class Organizations extends Component {
 
         const label = 'Organization' + (organizations.length > 1 ? 's' : '');
 
+        const coordinates = organizations.filter((organization) => {
+            return ! (organization.center === null || organization.center.coordinates === null);
+        }).map((organization)  => {
+            return [organization.center.coordinates.lat, organization.center.coordinates.long];
+        });
+
         return <MetadataItem label={label} className="Organizations">
             {organizations.map((organization, index) => {
                 return <Organization key={index} name={organization.name} url={organization.url} type={organization.type} center={organization.center} />
             })}
+
+            {coordinates.length > 0 && <div className="Map">
+                <Map
+                    center={getCenterFromDegrees(coordinates)}
+                    zoom={7}
+                    dragging={false}
+                    doubleClickZoom={false}
+                    keyboard={false}
+                    scrollWheelZoom={false}
+                    zoomControl={false}
+                    attributionControl={false}
+                >
+                    <TileLayer
+                        url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a>'
+                    />
+                    {coordinates.map((coordinate, index) => {
+                        return <Marker key={index} position={coordinate} />
+                    })}
+
+                    <AttributionControl position="bottomright" prefix={false} />
+                </Map>
+            </div>}
         </MetadataItem>
     }
 }
