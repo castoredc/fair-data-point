9a4af08eed339cfc183298d8e52c15238c225930
Martijn Kersloot <martijn@castoredc.com>
2020-04-01 13:54:14 +0200

    Add MethodType Type

--- src/Entity/Enum/StudyType.php
+++ src/Entity/Enum/StudyType.php
@@ -9,10 +9,8 @@ namespace App\Entity\Enum;
 /**
  * @ method static static interventional()
  * @ method static static observational()
- * @ method static static registry()
  * @ method bool isInterventional()
  * @ method bool isObservational()
- * @ method bool isRegistry()
  *
  * @inheritDoc
  */
@@ -20,5 +18,4 @@ class StudyType extends Enum
 {
     private const INTERVENTIONAL = 'interventional';
     private const OBSERVATIONAL = 'observational';
-    private const REGISTRY = 'registry';
 }
