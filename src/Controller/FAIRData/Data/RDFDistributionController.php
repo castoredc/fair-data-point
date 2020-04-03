952704f2c3fa3e732022d8f649d9e51c8a341299
Martijn Kersloot <martijn@castoredc.com>
2020-04-03 13:22:51 +0200

    Catch new Exceptions in RDFDistributionController

--- src/Controller/FAIRData/Data/RDFDistributionController.php
+++ src/Controller/FAIRData/Data/RDFDistributionController.php
@@ -9,14 +9,18 @@ use App\Entity\FAIRData\Catalog;
 use App\Entity\FAIRData\Dataset;
 use App\Entity\FAIRData\Distribution\Distribution;
 use App\Entity\FAIRData\Distribution\RDFDistribution\RDFDistribution;
+use App\Exception\NoAccessPermissionToStudy;
+use App\Exception\SessionTimedOut;
 use App\Message\Distribution\GetRecordCommand;
 use App\Message\Distribution\GetRecordsCommand;
 use App\Message\Distribution\RenderRDFDistributionCommand;
 use App\Security\CastorUser;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
+use Symfony\Component\Messenger\Exception\HandlerFailedException;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
@@ -41,32 +45,45 @@ class RDFDistributionController extends FAIRDataController
         /** @var CastorUser|null $user */
         $user = $this->getUser();
 
-        /** @var HandledStamp $handledStamp */
-        $handledStamp = $bus->dispatch(new GetRecordsCommand($dataset->getStudy(), $user))->last(HandledStamp::class);
+        try {
+            /** @var HandledStamp $handledStamp */
+            $handledStamp = $bus->dispatch(new GetRecordsCommand($dataset->getStudy(), $user))->last(HandledStamp::class);
 
-        /** @var Record[] $records */
-        $records = $handledStamp->getResult();
+            /** @var Record[] $records */
+            $records = $handledStamp->getResult();
 
-        /** @var HandledStamp $handledStamp */
-        $handledStamp = $bus->dispatch(new RenderRDFDistributionCommand($records, $distribution, $user))->last(HandledStamp::class);
-        $turtle = $handledStamp->getResult();
+            /** @var HandledStamp $handledStamp */
+            $handledStamp = $bus->dispatch(new RenderRDFDistributionCommand($records, $distribution, $user))->last(HandledStamp::class);
+            $turtle = $handledStamp->getResult();
 
-        if ($request->query->getBoolean('download') === true) {
-            $response = new Response($turtle);
-            $disposition = $response->headers->makeDisposition(
-                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-                $dataset->getStudy()->getSlug() . '_' . time() . '.ttl'
+            if ($request->query->getBoolean('download') === true) {
+                $response = new Response($turtle);
+                $disposition = $response->headers->makeDisposition(
+                    ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                    $dataset->getStudy()->getSlug() . '_' . time() . '.ttl'
+                );
+                $response->headers->set('Content-Disposition', $disposition);
+
+                return $response;
+            }
+
+            return new Response(
+                $turtle,
+                Response::HTTP_OK,
+                ['content-type' => 'text/turtle']
             );
-            $response->headers->set('Content-Disposition', $disposition);
+        } catch (HandlerFailedException $e) {
+            $e = $e->getPrevious();
 
-            return $response;
+            if ($e instanceof SessionTimedOut) {
+                return new JsonResponse($e->toArray(), 401);
+            }
+            if ($e instanceof NoAccessPermissionToStudy) {
+                return new JsonResponse($e->toArray(), 403);
+            }
+
+            return new JsonResponse([], 500);
         }
-
-        return new Response(
-            $turtle,
-            Response::HTTP_OK,
-            ['content-type' => 'text/turtle']
-        );
     }
 
     /**
@@ -86,31 +103,44 @@ class RDFDistributionController extends FAIRDataController
         /** @var CastorUser|null $user */
         $user = $this->getUser();
 
-        /** @var HandledStamp $handledStamp */
-        $handledStamp = $bus->dispatch(new GetRecordCommand($dataset->getStudy(), $record, $user))->last(HandledStamp::class);
+        try {
+            /** @var HandledStamp $handledStamp */
+            $handledStamp = $bus->dispatch(new GetRecordCommand($dataset->getStudy(), $record, $user))->last(HandledStamp::class);
 
-        /** @var Record[] $record */
-        $record = $handledStamp->getResult();
+            /** @var Record[] $record */
+            $record = $handledStamp->getResult();
 
-        /** @var HandledStamp $handledStamp */
-        $handledStamp = $bus->dispatch(new RenderRDFDistributionCommand($record, $distribution, $user))->last(HandledStamp::class);
-        $turtle = $handledStamp->getResult();
+            /** @var HandledStamp $handledStamp */
+            $handledStamp = $bus->dispatch(new RenderRDFDistributionCommand($record, $distribution, $user))->last(HandledStamp::class);
+            $turtle = $handledStamp->getResult();
 
-        if ($request->query->getBoolean('download') === true) {
-            $response = new Response($turtle);
-            $disposition = $response->headers->makeDisposition(
-                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
-                $dataset->getStudy()->getSlug() . '_' . time() . '.ttl'
+            if ($request->query->getBoolean('download') === true) {
+                $response = new Response($turtle);
+                $disposition = $response->headers->makeDisposition(
+                    ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                    $dataset->getStudy()->getSlug() . '_' . time() . '.ttl'
+                );
+                $response->headers->set('Content-Disposition', $disposition);
+
+                return $response;
+            }
+
+            return new Response(
+                $turtle,
+                Response::HTTP_OK,
+                ['content-type' => 'text/turtle']
             );
-            $response->headers->set('Content-Disposition', $disposition);
+        } catch (HandlerFailedException $e) {
+            $e = $e->getPrevious();
 
-            return $response;
+            if ($e instanceof SessionTimedOut) {
+                return new JsonResponse($e->toArray(), 401);
+            }
+            if ($e instanceof NoAccessPermissionToStudy) {
+                return new JsonResponse($e->toArray(), 403);
+            }
+
+            return new JsonResponse([], 500);
         }
-
-        return new Response(
-            $turtle,
-            Response::HTTP_OK,
-            ['content-type' => 'text/turtle']
-        );
     }
 }
