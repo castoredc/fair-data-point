9973005df580d0c6f652d443cc4ff5ecd9cd31f6
Martijn Kersloot <martijn@castoredc.com>
2020-04-07 10:08:30 +0200

    Add countDatasets function and add limits in findDatasets query

--- src/Repository/DatasetRepository.php
+++ src/Repository/DatasetRepository.php
@@ -12,6 +12,8 @@ use App\Entity\FAIRData\Department;
 use App\Entity\FAIRData\Organization;
 use App\Entity\Metadata\StudyMetadata;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
 use Doctrine\ORM\Query\Expr\Join;
 
 class DatasetRepository extends EntityRepository
@@ -23,18 +25,20 @@ class DatasetRepository extends EntityRepository
      *
      * @return mixed
      */
-    public function findDatasets(Catalog $catalog, ?string $search, ?array $studyType, ?array $methodType, ?array $country)
+    public function findDatasets(Catalog $catalog, ?string $search, ?array $studyType, ?array $methodType, ?array $country, ?int $perPage, ?int $page)
     {
+        $firstResult = $page !== null && $perPage !== null ? ($page - 1) * $perPage : 0;
+
         $qb = $this->getEntityManager()->createQueryBuilder()
-            ->select('dataset')
-            ->from(Dataset::class, 'dataset')
-            ->innerJoin('dataset.catalogs', 'catalog', Join::WITH, 'catalog.id = :catalog_id')
-            ->join(Study::class, 'study', Join::WITH, 'study.id = dataset.study')
-            ->join(StudyMetadata::class, 'metadata', Join::WITH, 'metadata.study = study.id')
-            ->leftJoin(StudyMetadata::class, 'metadata2', Join::WITH, 'metadata2.study = study.id AND metadata.created < metadata2.created')
-            ->setParameter('catalog_id', $catalog->getId())
-            ->where('metadata2.id IS NULL')
-            ->andWhere('dataset.isPublished = 1');
+                   ->select('dataset')
+                   ->from(Dataset::class, 'dataset')
+                   ->innerJoin('dataset.catalogs', 'catalog', Join::WITH, 'catalog.id = :catalog_id')
+                   ->join(Study::class, 'study', Join::WITH, 'study.id = dataset.study')
+                   ->join(StudyMetadata::class, 'metadata', Join::WITH, 'metadata.study = study.id')
+                   ->leftJoin(StudyMetadata::class, 'metadata2', Join::WITH, 'metadata2.study = study.id AND metadata.created < metadata2.created')
+                   ->setParameter('catalog_id', $catalog->getId())
+                   ->where('metadata2.id IS NULL')
+                   ->andWhere('dataset.isPublished = 1');
 
         if ($search !== null) {
             $qb->andWhere($qb->expr()->orX(
@@ -57,15 +61,78 @@ class DatasetRepository extends EntityRepository
 
         if ($country !== null) {
             $qb->innerJoin('metadata.centers', 'agent')
-                ->join(Department::class, 'department', Join::WITH, 'agent.id = department.id')
-                ->join(Organization::class, 'organization', Join::WITH, 'department.organization = organization.id')
-                ->andWhere('organization.country IN (:country)');
+               ->join(Department::class, 'department', Join::WITH, 'agent.id = department.id')
+               ->join(Organization::class, 'organization', Join::WITH, 'department.organization = organization.id')
+               ->andWhere('organization.country IN (:country)');
 
             $qb->setParameter('country', $country);
         }
 
         $qb->orderBy('metadata.briefName', 'ASC');
 
+        $qb->setFirstResult($firstResult);
+
+        if ($perPage !== null) {
+            $qb->setMaxResults($perPage);
+        }
+
         return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param StudyType[]|null  $studyType
+     * @param MethodType[]|null $methodType
+     * @param string[]|null     $country
+     */
+    public function countDatasets(Catalog $catalog, ?string $search, ?array $studyType, ?array $methodType, ?array $country): int
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder()
+                   ->select('count(dataset.id)')
+                   ->from(Dataset::class, 'dataset')
+                   ->innerJoin('dataset.catalogs', 'catalog', Join::WITH, 'catalog.id = :catalog_id')
+                   ->join(Study::class, 'study', Join::WITH, 'study.id = dataset.study')
+                   ->join(StudyMetadata::class, 'metadata', Join::WITH, 'metadata.study = study.id')
+                   ->leftJoin(StudyMetadata::class, 'metadata2', Join::WITH, 'metadata2.study = study.id AND metadata.created < metadata2.created')
+                   ->setParameter('catalog_id', $catalog->getId())
+                   ->where('metadata2.id IS NULL')
+                   ->andWhere('dataset.isPublished = 1');
+
+        if ($search !== null) {
+            $qb->andWhere($qb->expr()->orX(
+                $qb->expr()->like('metadata.briefName', ':search'),
+                $qb->expr()->like('metadata.briefSummary', ':search'),
+                $qb->expr()->like('metadata.summary', ':search')
+            ));
+            $qb->setParameter('search', '%' . $search . '%');
+        }
+
+        if ($studyType !== null) {
+            $qb->andWhere('metadata.type IN (:studyType)');
+            $qb->setParameter('studyType', $studyType);
+        }
+
+        if ($methodType !== null) {
+            $qb->andWhere('metadata.methodType IN (:methodType)');
+            $qb->setParameter('methodType', $methodType);
+        }
+
+        if ($country !== null) {
+            $qb->innerJoin('metadata.centers', 'agent')
+               ->join(Department::class, 'department', Join::WITH, 'agent.id = department.id')
+               ->join(Organization::class, 'organization', Join::WITH, 'department.organization = organization.id')
+               ->andWhere('organization.country IN (:country)');
+
+            $qb->setParameter('country', $country);
+        }
+
+        $qb->orderBy('metadata.briefName', 'ASC');
+
+        try {
+            return (int) $qb->getQuery()->getSingleScalarResult();
+        } catch (NoResultException $e) {
+            return 0;
+        } catch (NonUniqueResultException $e) {
+            return 0;
+        }
     }
 }
