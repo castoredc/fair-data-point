54dddf1a103cc77078f50cbc6cfbbc5656b4c541
Martijn Kersloot <martijn@castoredc.com>
2020-04-07 15:49:17 +0200

    Add clustering to map

--- src/js/components/DatasetMap/index.js
+++ src/js/components/DatasetMap/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {AttributionControl, Map, Marker, Popup, TileLayer} from "react-leaflet";
+import MarkerClusterGroup from 'react-leaflet-markercluster'
 import {getCenterFromDegrees, localizedText} from "../../util";
 
 import './DatasetMap.scss';
@@ -16,7 +17,7 @@ export default class DatasetMap extends Component {
         return <div className="DatasetMap">
             <Map
                 center={getCenterFromDegrees(coordinates)}
-                zoom={2}
+                zoom={3}
                 attributionControl={false}
             >
                 <TileLayer
@@ -24,19 +25,21 @@ export default class DatasetMap extends Component {
                     attribution='&copy; <a href="http://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap contributors</a>, &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a>'
                 />
 
-                {datasets.map((dataset, index) => {
-                    return <Marker key={index} position={[dataset.coordinates.lat, dataset.coordinates.long]}>
-                        <Popup>
-                            <Link to={dataset.relative_url} className="PopupDatasetTitle" target="_blank">
-                                <h3>{localizedText(dataset.title, 'en')}</h3>
-                            </Link>
-                            <div className="PopupOrganization">
-                                <strong>{dataset.organization}</strong><br />
-                                {dataset.city}, {dataset.country}
-                            </div>
-                        </Popup>
-                    </Marker>
-                })}
+                <MarkerClusterGroup>
+                    {datasets.map((dataset, index) => {
+                        return <Marker key={index} position={[dataset.coordinates.lat, dataset.coordinates.long]}>
+                            <Popup>
+                                <Link to={dataset.relative_url} className="PopupDatasetTitle" target="_blank">
+                                    <h3>{localizedText(dataset.title, 'en')}</h3>
+                                </Link>
+                                <div className="PopupOrganization">
+                                    <strong>{dataset.organization}</strong><br />
+                                    {dataset.city}, {dataset.country}
+                                </div>
+                            </Popup>
+                        </Marker>
+                    })}
+                </MarkerClusterGroup>
 
                 <AttributionControl position="bottomright" prefix={false} />
             </Map>
