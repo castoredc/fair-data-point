59344caff55dc3be8c6e19a6c6607a4407414efb
Martijn Kersloot <martijn@castoredc.com>
2020-06-11 16:27:47 +0200

    Add Version entity

---
+++ src/Entity/Enum/VersionType.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @phpcs:disable SlevomatCodingStandard.Classes.UnusedPrivateElements.UnusedConstant
+ */
+declare(strict_types=1);
+
+namespace App\Entity\Enum;
+
+/**
+ * @method static static major()
+ * @method static static minor()
+ * @method static static patch()
+ * @method bool isMajor()
+ * @method bool isMinor()
+ * @method bool isPatch()
+ *
+ * @inheritDoc
+ */
+class VersionType extends Enum
+{
+    private const MAJOR = 'major';
+    private const MINOR = 'minor';
+    private const PATCH = 'patch';
+}
