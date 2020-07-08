f09090905b874ffc4e3cf87448afa36e35b2d111
Martijn Kersloot <martijn@castoredc.com>
2020-07-08 09:23:33 +0200

    Update ORM mapping

--- src/Entity/Data/RDF/RDFDistribution.php
+++ src/Entity/Data/RDF/RDFDistribution.php
@@ -19,7 +19,7 @@ use Doctrine\ORM\Mapping as ORM;
 class RDFDistribution extends DistributionContents implements AccessibleEntity
 {
     /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Data\DataModel\DataModel")
+     * @ORM\ManyToOne(targetEntity="App\Entity\Data\DataModel\DataModel", inversedBy="distributions")
      * @ORM\JoinColumn(name="data_model", referencedColumnName="id", nullable=false)
      *
      * @var DataModel
