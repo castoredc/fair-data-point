5e325d13d8cd0c161bc6488429f7f6e539c25ffd
Martijn Kersloot <martijn@castoredc.com>
2020-07-23 13:29:41 +0200

    Code style fixes

--- src/Entity/Castor/Data/InstanceData.php
+++ src/Entity/Castor/Data/InstanceData.php
@@ -5,7 +5,6 @@ namespace App\Entity\Castor\Data;
 
 use App\Entity\Castor\Instances\Instance;
 use App\Entity\Castor\Record;
-use Doctrine\Common\Collections\ArrayCollection;
 
 class InstanceData extends RecordData
 {
@@ -15,15 +14,11 @@ class InstanceData extends RecordData
     /** @var Instance */
     protected $instance;
 
-    /** @var ArrayCollection<string, FieldResult> */
-    private $data;
-
     public function __construct(Record $record, Instance $instance)
     {
         parent::__construct($record);
 
         $this->instance = $instance;
-        $this->data = new ArrayCollection();
     }
 
     public function getInstance(): Instance
