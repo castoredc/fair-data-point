263617caf3899f21f406413999e01e4cbf6e42be
Martijn Kersloot <martijn@castoredc.com>
2020-07-31 09:20:52 +0200

    Allow the use of Institute details as Literal

--- src/Entity/Enum/RecordDetailLiterals.php
+++ src/Entity/Enum/RecordDetailLiterals.php
@@ -12,13 +12,15 @@ namespace App\Entity\Enum;
  * @method static static instituteName()
  * @method static static instituteAbbreviation()
  * @method static static instituteCode()
- * @method static static instituteCountryId()
+ * @method static static instituteCountryCode()
+ * @method static static instituteCountryName()
  * @method bool isRecordId()
  * @method bool isInstituteId()
  * @method bool isInstituteName()
  * @method bool isInstituteAbbreviation()
  * @method bool isInstituteCode()
- * @method bool isInstituteCountryId()
+ * @method bool isInstituteCountryCode()
+ * @method bool isInstituteCountryName()
  * @inheritDoc
  */
 class RecordDetailLiterals extends Enum
@@ -28,5 +30,6 @@ class RecordDetailLiterals extends Enum
     public const INSTITUTE_NAME = '##institute_name##';
     public const INSTITUTE_ABBREVIATION = '##institute_abbreviation##';
     public const INSTITUTE_CODE = '##institute_code##';
-    public const INSTITUTE_COUNTRY_ID = '##institute_country_id##';
+    public const INSTITUTE_COUNTRY_CODE = '##institute_country_code##';
+    public const INSTITUTE_COUNTRY_NAME = '##institute_country_name##';
 }
