cc5dbb526ba6476b692016908ab5f44cad9b65d8
Martijn Kersloot <martijn@castoredc.com>
2020-09-29 12:11:29 +0200

    Add deploy script for .dev

---
+++ config/dev/deploy.php
@@ -0,0 +1,144 @@
+<?php
+
+use EasyCorp\Bundle\EasyDeployBundle\Deployer\DefaultDeployer;
+use Cz\Git\GitRepository;
+use EasyCorp\Bundle\EasyDeployBundle\Context;
+use EasyCorp\Bundle\EasyDeployBundle\Server\Property;
+use EasyCorp\Bundle\EasyDeployBundle\Server\Server;
+use Symfony\Component\Console\Helper\QuestionHelper;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
+use Symfony\Component\Console\Question\Question;
+
+return new class extends DefaultDeployer
+{
+    /**
+     * @var GitRepository
+     */
+    private $gitRepo;
+
+    /** @var QuestionHelper */
+    private $questionHelper;
+
+    public function initialize(Context $context): void
+    {
+        parent::initialize($context);
+
+        $question = new ConfirmationQuestion('Are you sure you want to deploy? [y/N] ', false);
+
+        if (!$this->ask($question)) {
+            $this->log('<h1><fg=red>Not deploying</fg></h1>');
+            exit;
+        }
+    }
+
+    public function configure()
+    {
+        $this->questionHelper = new QuestionHelper();
+        $this->gitRepo = new GitRepository($this->getContext()->getLocalProjectRootDir());
+
+        $localBranch = $this->gitRepo->getCurrentBranchName();
+        $question = new Question(sprintf('Which branch do you want to deploy? [%s]', $localBranch), $localBranch);
+
+        $branch = $this->ask($question);
+
+        return $this->getConfigBuilder()
+            ->server('fdp.castoredc.dev')
+            ->deployDir('/srv/www/fdp.castoredc.dev')
+            ->repositoryUrl(sprintf('git@github.com:%s.git', 'castoredc/fair-data-point'))
+            ->repositoryBranch($branch)
+            ->composerInstallFlags('--prefer-dist --no-interaction --no-dev --no-scripts --quiet')
+        ;
+    }
+
+    public function beforeStartingDeploy()
+    {
+        $branchToDeploy = $this->getConfig('repositoryBranch');
+
+        $this->gitRepo->fetch();
+        $localBranch = $this->gitRepo->getCurrentBranchName();
+
+        if ($localBranch !== $branchToDeploy) {
+            throw new Exception(sprintf(
+                "You are trying to deploy branch '%s' your branch is currently set to '%s'.\n" .
+                "We need to build webpack locally, therefore we require that you are on the same branch\n" .
+                'And that its up to date with the remote.',
+                $branchToDeploy,
+                $localBranch
+            ));
+        }
+
+//        if ($this->gitRepo->hasChanges()) {
+//            throw new Exception(sprintf(
+//                "You have un-pushed local changes.\n" .
+//                'We need to build webpack locally, therefore we require that you are up to date with the remote.',
+//                $branchToDeploy,
+//                $localBranch
+//            ));
+//        }
+//
+//        $this->log('<h1>Pulling in changes from remote git</h1>');
+//        $this->gitRepo->pull();
+    }
+
+    public function beforePreparing()
+    {
+        $this->log('<h2>Copy ENV files</h2>');
+        $this->runRemote(sprintf('cp -RPp {{ deploy_dir }}/repo/.env {{ project_dir }}'));
+        $this->runRemote(sprintf('cp -RPp {{ deploy_dir }}/.env.local {{ project_dir }}'));
+    }
+
+    public function beforePublishing()
+    {
+        /** @var Server $server */
+        $server = $this->getServers()->findAll()[0];
+        $projectDir = $server->get(Property::project_dir);
+
+        $this->log('<h1>Building Ui</h1>');
+        $this->runLocal('yarn install');
+        $this->runLocal('yarn run encore production');
+        $this->runLocal(sprintf(
+            'rsync --progress -crDpLt --force --delete ./public/build/ %s:%s/public/build/',
+            $server->getHost(),
+            $projectDir
+        ));
+
+        $this->handleMigrations();
+
+        $version = $this->getCurrentVersionFromProjectDir($projectDir);
+
+//        $tag = sprintf('production_%s', $version);
+//
+//        $this->gitRepo->createTag($tag);
+//        $this->gitRepo->push(null, ['origin', $tag]);
+//
+//        $this->gitRepo->createTag('production', '--force');
+//        $this->gitRepo->push(null, ['origin', 'production', '--force']);
+
+        $this->pushVersion($version);
+    }
+
+    public function pushVersion($version): void
+    {
+        $this->runRemote(
+            sprintf('cd {{ project_dir }} && echo "APP_VERSION=%s" > ./.env.prod.local', $version)
+        );
+    }
+
+    private function getCurrentVersionFromProjectDir(string $projectDir): string
+    {
+        $pathParts = explode('/', $projectDir);
+
+        return end($pathParts);
+    }
+
+    private function handleMigrations(): void
+    {
+        $this->log('<h1>Running migrations</h1>');
+        $this->runRemote('bin/console d:m:m --no-interaction --quiet');
+    }
+
+    private function ask(Question $question)
+    {
+        return $this->questionHelper->ask($this->getContext()->getInput(), $this->getContext()->getOutput(), $question);
+    }
+};
