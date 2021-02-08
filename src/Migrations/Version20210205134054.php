f2858e414bad3892398ed2e4b8a53401911bfacb
Martijn Kersloot <martijn@castoredc.com>
2021-02-08 11:24:12 +0100

    Fix code style on Migration

--- src/Migrations/Version20210205134054.php
+++ src/Migrations/Version20210205134054.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace DoctrineMigrations;
@@ -12,12 +11,12 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20210205134054 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE data_transformation_mapping_element (data_transformation_mapping_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', element_id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', INDEX IDX_19FCE9EA83FD70F0 (data_transformation_mapping_id), INDEX IDX_19FCE9EA1F1F2A24 (element_id), PRIMARY KEY(data_transformation_mapping_id, element_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
@@ -28,7 +27,7 @@ final class Version20210205134054 extends AbstractMigration
         $this->addSql('ALTER TABLE data_specification_mappings ADD syntax LONGTEXT DEFAULT NULL');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE data_transformation_mapping_element');
