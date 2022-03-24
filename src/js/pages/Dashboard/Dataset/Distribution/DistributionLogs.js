864f944662f0d109b9fad796f8797705970d95a2
Martijn Kersloot <martijn@castoredc.com>
2022-03-24 14:59:49 +0100

    Update DistributionLogs.js

--- src/js/pages/Dashboard/Dataset/Distribution/DistributionLogs.js
+++ src/js/pages/Dashboard/Dataset/Distribution/DistributionLogs.js
@@ -4,11 +4,13 @@ import PageBody from "components/Layout/Dashboard/PageBody";
 
 export default class DistributionLogs extends Component {
     render() {
-        const {dataset, distribution, history} = this.props;
+        const {dataset, study, catalog, distribution, history} = this.props;
 
         return <PageBody>
             <DistributionLogsDataTable
                 history={history}
+                catalog={catalog}
+                study={study}
                 dataset={dataset}
                 distribution={distribution}
             />
