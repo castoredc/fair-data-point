3b2bcc76ea2a3496937fa986e29bf70ce552588a
rohit-castoredc <rohit.malhotra@castoredc.com>
2022-04-05 17:44:06 +0530

    FDP-15 pr fixed

--- src/js/network/apiClient.ts
+++ src/js/network/apiClient.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosError } from "axios";
 
+const isLocalEnv = process.env.NODE_ENV === "development";
+
 /**
  * Create an Axios Client with defaults
  */
@@ -8,25 +10,27 @@ const apiClient = axios.create();
 apiClient.interceptors.response.use(
   (response) => {
     // Only logs the response when user is accessing console in development mode
-    if (process.env.NODE_ENV === "development") {
+    if (isLocalEnv) {
       console.log(response);
     }
     return response;
   },
   (error: AxiosError) => {
     if (error.response) {
-      // Request was made but server responded with something other than 2xx
-      console.error("Status:", error.response.status);
-      console.error("Data:", error.response.data);
+      if (isLocalEnv) {
+        // Request was made but server responded with something other than 2xx
+        console.error("Status:", error.response.status);
+        console.error("Data:", error.response.data);
+      }
 
-      // Logout the user if the authorization fails
+      // Redirect the user to login page if the authorization fails
       if (error.response.status === 401) {
         window.location.href =
           "/login?path=" + encodeURIComponent(window.location.pathname);
       }
     } else {
       // Something else happened while setting up the request triggered the error
-      console.error("Error Message:", error.message);
+      isLocalEnv && console.error("Error Message:", error.message);
     }
     return Promise.reject(error);
   }
