870e85ad3613eec5b273203a51b3e807a4eb2de5
rohit-castoredc <rohit.malhotra@castoredc.com>
2022-04-08 15:37:57 +0530

    FDP-15 PR fixes

--- src/js/network/apiClient.ts
+++ src/js/network/apiClient.ts
@@ -9,10 +9,6 @@ const apiClient = axios.create();
 
 apiClient.interceptors.response.use(
   (response) => {
-    // Only logs the response when user is accessing console in development mode
-    if (isLocalEnv) {
-      console.log(response);
-    }
     return response;
   },
   (error: AxiosError) => {
